22e58251c94dcfcbce31ecf81afaa309f6733678
Alexey <Lexa070301@mail.ru>
2021-01-04 14:39:30 +0300

    update database

--- coworking/apps/payments/models.py
+++ coworking/apps/payments/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Payment(models.Model):
-    date = models.DateTimeField('Дата оплаты')
-    size = models.IntegerField('Размер оплаты')
+    date = models.DateTimeField('Дата оплаты', default='')
+    size = models.IntegerField('Размер оплаты', default=0)
     booking = models.ForeignKey('bookings.Booking', on_delete=models.CASCADE)
     def __str__(self):
         return self.date
@@ -12,7 +12,7 @@ class Payment(models.Model):
         verbose_name_plural = 'Оплаты'
 
 class Payment_status(models.Model):
-    status = models.CharField('Статус', max_length=15)
+    status = models.CharField('Статус', max_length=15, default='')
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
 
     def __str__(self):
