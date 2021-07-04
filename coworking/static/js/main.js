d4ee6a808a032592fe4c9775f53edcb45d548b75
Lexa070301 <lexa070301@gmail.com>
2021-07-04 15:53:45 +0300

    many updates

---
+++ coworking/static/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    var mobyMenu = new Moby({
+        menu: $('#main-nav'), // The menu that will be cloned
+        mobyTrigger: $('#moby-button'), // Button that will trigger the Moby menu to open
+    });
+})
